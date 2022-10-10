6b6050ec6efe539fdcf288fd95da54beb9447773
Ashraf59 <ashrafams59b@gmail.com>
2022-10-11 01:37:48 +0600

    Nav Bar completed with logo

---
+++ src/Utilities/routes.js
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../components/Root";
+import Home from "../components/Home"
+import Topics from "../components/Topics";
+import Statistics from "../components/Statistics";
+import Blog from "../components/Blog";
+
+
+const router = createBrowserRouter([
+
+    {
+        path: '/',
+        element: <Root></Root>,
+        
+        
+        children: [
+          {
+              path: '/',
+              element: <Home></Home>
+          },
+          {
+              path: '/home',
+              element: <Home></Home>
+          },
+          {
+              path: '/topics',
+              element: <Topics></Topics>
+          },
+          {
+              path: '/statistics',
+              element: <Statistics></Statistics>
+          },
+          {
+              path: '/blog',
+              element: <Blog></Blog>
+          }
+        ]
+      }
+      
+])
+export default router;
