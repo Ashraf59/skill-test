0a68574281c98c3a1a422ee054e636890ffefc4e
Ashraf59 <ashrafams59b@gmail.com>
2022-10-11 17:43:11 +0600

    map on Quiz Option and design option by Tailwind

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Option = ({option}) => {
     return (
         <div>
-            <p>Option: {option}</p>
+             
+       <button
+            type='button'
+            className='px-8 block w-full mt-4 py-3 font-semibold rounded-full bg-teal-300 text-gray-800 hover:bg-cyan-400'
+          >
+            <p>{option}</p>
+          </button>
         </div>
     );
 };
--- src/components/Topics.js
+++ src/components/Topics.js
@@ -6,25 +6,29 @@ const Topics = ({quiz}) => {
     // const quizes = useLoaderData()
     console.log(quiz)
     const {id, options, correctAnswer, question } = quiz;
+    
 
     return (
-     <div className='bg-gray-100 p-6 rounded shadow-lg'>
-           <div className='mb-2 text-xl font-bold leading-none sm:text-2xl'>
-           <p>Question: {question}</p>
+     <div>
+           
+     <div className=' p-12 rounded shadow-lg w-2/4 m-auto'>
+           <div className='mb-5 text-xl font-bold leading-none sm:text-2xl my-15 bg-gray-100 p-12'>
+           <p className='text-center mb-8'>Question:  {question}</p>
             
-            <p>Options: <li>{options}</li></p>
-            <p>Correct Answer: {correctAnswer}</p>
-
+            <div className='m-auto'>
+                
             {
                 options.map(option => <Option
                 option = {option}
                 ></Option>)
             }
+            </div>
             
 
            </div>
            
       </div>
+     </div>
     );
 };
 
