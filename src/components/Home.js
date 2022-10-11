fdca8c4d47b1c115e835eeeca21873636389586d
Ashraf59 <ashrafams59b@gmail.com>
2022-10-11 09:31:13 +0600

    Home Image and Text completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import HeroImage from "../assets/hero-1.jpg"
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default Home;
+        <section>
+          <div className="bg-gray-100">
+            <div className="container flex flex-col items-center px-4 py-12 pb-12 mx-auto text-center lg:pb-56 md:py-16 md:px-8 lg:px-32 text-gray-900">
+              <h1 className="text-2xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-gray-800">
+                Programming Quiz Test
+              </h1>
+              <p className="mt-6 mb-2 text-lg sm:mb-12 xl:max-w-3xl text-gray-900">
+                This is a quiz contest to justify your skills on progrmming,
+                Please selecet your programming language and start the skills test.
+              </p>
+              
+            </div>
+          </div>
+          <img
+            src={HeroImage}
+            alt=""
+            className="mx-auto mb-12 -mt-5 rounded-lg shadow-md lg:-mt-40 bg-gray-500"
+          />
+        </section>
+      );
+    };
+    
+    export default Home;
+    
