aef4b6820a853d1920ae42cd77b232e9cb29f919
Ashraf59 <ashrafams59b@gmail.com>
2022-10-12 12:23:01 +0600

    Toastify Completed

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Option = ({option}) => {
+const Option = ({option, handleAnswer}) => {
     return (
         <div className='grid grid-cols-1 md:grid-cols-1 md:w-full'>
-            <button
+            <button onClick={()=> handleAnswer(option)}
               type='button'
               className='px-8 block m-auto mt-4 py-3 font-semibold rounded-full bg-teal-300 text-gray-900 hover:bg-cyan-400 hover:text-white'
             >
--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import Option from './Option';
-import './Topics.css'
+import { toast } from 'react-toastify'
+import './Topics.css';
+import { EyeSlashIcon } from '@heroicons/react/24/solid';
 // import { useLoaderData } from 'react-router-dom';
 
 const Topics = ({quiz}) => {
     // const quizes = useLoaderData()
-    console.log(quiz)
+    // console.log(quiz)
     const {id, options, correctAnswer, question } = quiz;
-    
+
+    const handleAnswer = (option) =>{
+        if(option === correctAnswer){
+            toast.success('Correct Answer', {autoClose: 500})
+
+        }
+        else {
+            toast.error('Wrong Answer', {autoClose: 500})
+
+        }
+    }
+
+    const handleIcon = (id) => {
+        toast(`${correctAnswer}`, { autoClose: 1000 })
+    }
 
     return (
      <div className='quiz'>
         <p className='text-center text-2xl mb-6 font-semibold text-gray-600'>Question:  {question}</p>
+        <p><span onClick={() => handleIcon(id)}><EyeSlashIcon className="h-6 w-6 text-blue-500"/></span></p>
         <div className='m-auto'>  
             {
                 options.map(option => <Option
                 option = {option}
+                handleAnswer = {handleAnswer}
                 ></Option>)
             }
         </div>
+
      </div>
     );
 };
